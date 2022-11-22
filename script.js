4658f75594f6a4b700e082d09526f7ff8918f6b8
CheeseburgerIM <13051172893@163.com>
2022-11-22 21:41:35 +0800

    Site updated: 2022-11-22 21:41:35

---
+++ script.js
@@ -0,0 +1,26 @@
+window.onload = () => {
+    var master = new Date("2022/12/24")
+    var current = new Date()
+    var value = (master.getTime() - current.getTime()) / (24 * 3600 * 1000)
+    var scores = {
+        math: 115,
+        datastructure: 115,
+        english: 70,
+        politics: 60
+    }
+    var total = 0;
+    for (let item in scores) {
+        total = total + scores[item]
+    }
+    document.body.innerHTML =
+        `
+    <master>距离考研还剩 ${Math.floor(value)} 天</master>
+    <ul>
+        <li>数一： ${scores.math}</li>
+        <li>英一： ${scores.english}</li>
+        <li>数据结构： ${scores.datastructure}</li>
+        <li>政治： ${scores.politics}</li>
+        <li><b>总分： ${total}</b></li>
+    </ul>
+    `
+}
